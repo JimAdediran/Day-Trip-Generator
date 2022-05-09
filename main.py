d6e8ac3077cf54335fcff2ee4f5ba6c4a9924631
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-09 00:48:04 -0400

    Function added to main.py for choosing destination

--- main.py
+++ main.py
@@ -6,4 +6,21 @@ entertainments = ["Karoake", "Touring Ellis Island", "Movies"]
 import random
 
 print("Welcome to the Day Trip Generator, if you're not sure what you would like to do for your trip you have come to the right place!")
-print(f"We have selected {random.choice(destinations)} ")
+
+
+def choose_destination():
+    destination_one = random.choice(destinations)
+    print(f"We have selected {destination_one} for your destination! Does this sound good? Enter y/n:")
+    destinations.remove(destination_one)
+    user_input = input()
+    if user_input == "n":
+        destination_two = random.choice(destinations)
+        print(f"Oh, sorry you don't like this destination. No worries, we can try something else! How about {destination_two}? Enter y/n:")
+        destinations.remove(destination_two)
+        user_input_two = input()
+        if user_input_two == "n":
+            print(f"Oh, sorry you dont like this destination. No worries, we can try something else! There is also {destinations[0]}? Enter y/n:")
+            user_input_three = input()
+
+choose_destination()
+        
