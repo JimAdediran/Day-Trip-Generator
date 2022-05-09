e693342912a7cb6328f2fd47d52a471b86ea43be
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-08 23:18:11 -0400

    Text print to console in main.py asking about destination and json file generated

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ entertainments = ["Karoake", "Touring Ellis Island", "Movies"]
 
 import random
 
+print("Welcome to the Day Trip Generator, if you're not sure what you would like to do for your trip you have come to the right place!")
+print(f"We have selected {random.choice(destinations)} ")
