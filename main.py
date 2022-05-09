f28fdb06f28f1e056fd317df439795238146d03a
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-08 23:03:59 -0400

    Lists for destinations, restaurants, modes of transportation, entertainments

---
+++ main.py
@@ -0,0 +1,4 @@
+destinations = ["Tokyo", "Boston", "Paris"]
+restaurants = ["Chicken", "Burgers", "Turkey Spot"]
+modes_of_transportation = ["Rental Car", "Train", "Plane"]
+entertainments = ["Karoake", "Touring Ellis Island", "Movies"]
