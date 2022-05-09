4b0bcc3ea6c1a176e5c58f5cee039e2958f82756
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-05-09 00:58:01 -0400

    Function for chooseing transportation added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 destinations = ["Tokyo", "Boston", "Paris"]
 restaurants = ["Chicken", "Burgers", "Turkey Spot"]
-modes_of_transportation = ["Rental Car", "Train", "Plane"]
+transportations = ["Rental Car", "Train", "Plane"]
 entertainments = ["Karoake", "Touring Ellis Island", "Movies"]
 
 import random
@@ -38,4 +38,20 @@ def choose_restaurant():
             print(f"Oh, sorry you dont like this restaurant. No worries, we can try something else! There is also {restaurants[0]}? Enter y/n:")
             user_input_three = input()
        
-choose_restaurant
+choose_restaurant()
+
+def choose_transportation():
+    transportation_one = random.choice(transportations)
+    print(f"We have selected {transportation_one} for your restaurant! Does this sound good? Enter y/n:")
+    destinations.remove(transportation_one)
+    user_input = input()
+    if user_input == "n":
+        transportation_two = random.choice(destinations)
+        print(f"Oh, sorry you don't like this restaurant. No worries, we can try something else! How about {transportation_two}? Enter y/n:")
+        destinations.remove(transportation_two)
+        user_input_two = input()
+        if user_input_two == "n":
+            print(f"Oh, sorry you dont like this restaurant. No worries, we can try something else! There is also {transportations[0]}? Enter y/n:")
+            user_input_three = input()
+
+choose_transportation()
